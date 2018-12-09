6c6c72aa42319157265a526767b35b04d7e7f097
Vinoth Kumar Asaithambi <me@geekyvin.com>
2018-12-09 23:21:57 +0000

    init-commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-pci
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_pci/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-pci.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-pci.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
